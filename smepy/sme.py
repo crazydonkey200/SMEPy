6b6e0e2b24c82190777b7a5220d0f36bbc4aa29a
Liang Chen <crazydonkey200@gmail.com>
2015-06-08 19:20:57 -0500

    In struct_case:
    (1) added support for nested expressions (expressions that
    has other expressions as arguments)
    
    (2) changed StructCase to just maintain one dictionary for
    both expression and entity
    
    (3) added contains and getitem in StructCase to support
    retrieve by name and test for containment by name or object
    itself for entity and expression.
    
    In sme:
    
    (1) added connect_matches to connect parent match with child
    match
    
    (2) added consistency propagation to filter out
    inconsistent and incomplete matches and build kernel
    mappings along the way.
    
    (3) added topological_sort as utility function.

--- smepy/sme.py
+++ smepy/sme.py
@@ -1,4 +1,4 @@
-#import struct_case as sc
+import struct_case as sc
 
 class SME:
     """The main class that holds all
@@ -7,8 +7,78 @@ class SME:
 
 class Mapping:
     """ """
-    pass
+    def __init__(self, matches=None):
+        self.base_to_target = {}
+        self.target_to_base = {}
+        self.matches = set()
+        if matches:
+            self.add_all(matches)
+
+    def get_mapped_base(self, base):
+        if base in self.base_to_target:
+            return self.base_to_target[base]
+        else:
+            return None
+
+    def get_mapped_target(self, target):
+        if target in self.target_to_base:
+            return self.target_to_base[target]
+        else:
+            return None
+
+    def is_consistent_with(self, match):
+        base = match.base
+        target = match.target
+        corresponding_target = self.get_mapped_base(base)
+        corresponding_base = self.get_mapped_target(target)
+        is_base_consistent = (not corresponding_target) or \
+            (corresponding_target == target)
+        is_target_consistent = (not corresponding_base) or \
+            (corresponding_base == base)
+        return is_base_consistent and is_target_consistent
+        
+    def mutual_consistent(self, mapping):
+        for match in mapping.matches:
+            if not self.is_consistent_with(match):
+                #print 'Mapping is not consistent with', match
+                return False
+        return True 
     
+    def add(self, match, check_consistency=True):
+        if match in self.matches:
+            return 
+        elif (not check_consistency) or self.is_consistent_with(match):
+            base = match.base
+            target = match.target
+            self.base_to_target[base] = target
+            self.target_to_base[target] = base
+            self.matches.add(match)
+        else:
+            print 'Mapping is not consistent with', match
+            raise ValueError
+
+    def add_all(self, matches, check_consistency=True):
+        for match in matches:
+            self.add(match, check_consistency)
+
+    def merge(self, mapping):
+        # consistency should be checked before merging
+        self.add_all(mapping.matches, check_consistency=False) 
+            
+    def __str__(self):
+        entity_matches = []
+        expression_matches = []
+        for match in self.matches:
+            if isinstance(match.base, sc.Expression):
+                expression_matches.append(match)
+            elif isinstance(match.base, sc.Expression):
+                entity_matches.append(match)
+        expression_matches_str = \
+            ',\n'.join(map(repr, expression_matches))
+        entity_matches_str = ', '.join(map(repr, entity_matches))
+        return 'expression mappings:\n' + expression_matches_str + \
+            '\n' + 'entity mappings:\n' + entity_matches_str    
+        
 class Match:
     """ """
     def __init__(self, base, target, score=0.0):
@@ -17,17 +87,28 @@ class Match:
         self.score = score
         self.children = []
         self.parents = []
+        self.mapping = None
+        self.is_incomplete = False
+        self.is_inconsistent = False
         
+    def add_parent(self, parent):
+        self.parents.append(parent)
+
+    def add_child(self, child):
+        self.children.append(child)
+
     def __repr__(self):
         return '('+repr(self.base)+' -- '+repr(self.target)+')'        
 
     def __eq__(self, other):
-        return isinstance(other, Match) and (self.base == other.base) and \
+        return isinstance(other, Match) and\
+            (self.base == other.base) and \
             (self.target == other.target)
 
     def __hash__(self):
         return hash(repr(self))
-        
+
+    
 class Params:
     """ """
     pass
@@ -41,23 +122,91 @@ def predicate_similarity(pred_1, pred_2):
 def are_predicates_matchable(pred_1, pred_2):
     return pred_1.name == pred_2.name    
 
+def are_matchable(item_1, item_2):
+    is_exp_1 = isinstance(item_1, sc.Expression)
+    is_exp_2 = isinstance(item_2, sc.Expression)
+    if is_exp_1 and is_exp_2:
+        return are_predicates_matchable(item_1.predicate,
+                                        item_2.predicate)
+    elif (not is_exp_1) and (not is_exp_2):
+        return True
+    else:
+        return False
+    
 # need to change the pair stuff into match stuff
 def create_all_possible_matches(case_1, case_2):
     exp_list_1 = case_1.expression_list
     exp_list_2 = case_2.expression_list
-    pairs = set()
+    matches = set()
     for exp_1 in exp_list_1:
         for exp_2 in exp_list_2:
-            new_pairs = pair_expression(exp_1, exp_2)
-            pairs = set.union(pairs, new_pairs)
-    return [Match(pair[0], pair[1]) for pair in pairs]
+            new_matches = match_expression(exp_1, exp_2)
+            matches = set.union(matches, new_matches)
+    return list(matches)
     
-def pair_expression(exp_1, exp_2):
+def match_expression(exp_1, exp_2):
     pred_1 = exp_1.predicate
     pred_2 = exp_2.predicate
     args_1 = exp_1.args
     args_2 = exp_2.args
-    if are_predicates_matchable(pred_1, pred_2):
-        return set([(exp_1, exp_2)] + zip(args_1, args_2))
+    pair_list = [(exp_1, exp_2)] + zip(args_1, args_2)
+    if all([are_matchable(pair[0], pair[1]) for pair in pair_list]):
+        match_list = [Match(pair[0], pair[1]) for pair in pair_list]
+        return set(match_list)
     else:
-        return set()    
+        return set()
+
+def connect_matches(matches):
+    match_dict = {}
+    for match in matches:
+        match_dict[(match.base, match.target)] = match
+    for match in matches:
+        base = match.base
+        target = match.target
+        if isinstance(base, sc.Expression):
+            arg_pair_list = zip(base.args, target.args)
+            for arg_pair in arg_pair_list:
+                if arg_pair in match_dict:
+                    child_match = match_dict[arg_pair]
+                    child_match.add_parent(match)
+                    match.add_child(child_match)
+                else:
+                    match.is_incomplete = True
+
+def consistency_propagation(matches):
+    match_graph = dict([(match, match.children) for match in matches])
+    sorted_from_leaves_matches = topological_sort(match_graph)
+    # to be continued
+    for match in sorted_from_leaves_matches:
+        match.mapping = Mapping([match] + match.children)
+        for child in match.children:
+            if match.mapping.mutual_consistent(child.mapping):
+                match.mapping.merge(child.mapping)
+            else:
+                match.is_inconsistent = True
+                break
+
+def topological_sort(graph_dict):
+    """Input: graph represented as
+    {node: [next_node_1, next_nodet_2], ...}"""
+    sorted_list = []
+    sorted_set = set()
+    new_graph_dict = graph_dict.copy()
+    while new_graph_dict:
+        for node in new_graph_dict:
+            next_node_list = new_graph_dict[node]
+            if all([(next_node in sorted_set) \
+                    for next_node in next_node_list]):
+                sorted_list.append(node)
+                sorted_set.add(node)
+                del new_graph_dict[node]
+                break
+        else:
+            print 'Cyclic graph!'
+            return
+    return sorted_list    
+                    
+#test examples
+ms_1 = create_all_possible_matches(sc.water_flow, sc.heat_flow)
+connect_matches(ms_1)
+consistency_propagation(ms_1)
--- smepy/struct_case.py
+++ smepy/struct_case.py
@@ -1,43 +1,108 @@
 import copy
 
+def get_hash_name(item):
+    if isinstance(item, list):
+        return '(' + ' '.join(map(get_hash_name, item)) + ')'
+    else:
+        return item
+
 class StructCase:
     """A case is a nugget containing entities and expressions."""
     def __init__(self, s_exp_w_list):
-        self.entities = {}
-        self.expressions = {}
+        self.items = {}
         for s_exp_w in s_exp_w_list:
-            s_exp, w = s_exp_w
-            args = s_exp[1:]
-            for arg in args:
-                if not arg in self.entities:
-                    ent = Entity(arg)
-                    self.entities[ent.name] = ent
-            exp = Expression(self, s_exp, w)
-            self.expressions[exp.name] = exp
+            self.add_s_exp_w(s_exp_w)
         self.vocab = current_vocab
-
+    
     @property
     def expression_list(self):
-        return [self.expressions[key] for key in list(self.expressions)]
+        return [self.items[key] \
+                for key in list(self.items) \
+                if isinstance(self.items[key], Expression)]
 
     @property
     def entity_list(self):
-        return [self.entities[key] for key in list(self.entites)]
+        return [self.items[key] \
+                for key in list(self.items) \
+                if isinstance(self.items[key], Entity)]
 
-    def __repr__(self):
-        exps_str = '\n '.join([repr(self.expressions[key]) for key in list(self.expressions)])
+    @property
+    def item_list(self):
+        return [self.items[key] for key in list(self.items)]
+
+    def __getitem__(self, index):
+        new_index = index
+        if isinstance(index, list):
+            new_index = get_hash_name(index)
+        if new_index in self:
+            return self.items[new_index]
+        else:
+            return None
+    
+    def add(self, item):
+        if not item in self:
+            if isinstance(item, list):
+                return self.add_s_exp_w((item, 1.0))
+            elif isinstance(item, tuple):
+                return self.add_s_exp_w(item)
+            elif isinstance(item, str):
+                return self.add_entity(Entity(item))
+            elif isinstance(item, Expression):
+                return self.add_expression(item)
+            elif isinstance(item, Entity):
+                return self.add_entity(item)
+        elif isinstance(item, Expression) or isinstance(item, Entity):
+            return item
+        else:
+            return self[item]
+
+    def add_s_exp_w(self, s_exp_w):
+        s_exp, w = s_exp_w
+        new_expression = Expression(self, s_exp, w)
+        self.items[new_expression.name] = new_expression
+        return new_expression
+
+    def add_entity(self, entity):
+        self.items[entity.name] = entity
+        return entity
+
+    def add_expression(self, expression):
+        self.items[expression.name] = expression
+        for arg in expression.args:
+            self.add(arg)
+        return expression
+        
+    def __contains__(self, item):
+        if isinstance(item, Expression) or \
+           isinstance(item, Entity):
+            return item.name in self.items
+        elif isinstance(item, list):
+            return get_hash_name(item) in self.items
+        else:
+            return item in self.items
+
+    def __deepcopy__(self, memo):
+        return StructCase([(expression.list_form, expression.weight) \
+                           for expression in self.expression_list])
+        
+    def __str__(self):
+        exps_str = '\n '.join([repr(expression) \
+                               for expression in self.expression_list])
         exps_str = '(' + exps_str + ')'
-        ents_str = ', '.join([repr(self.entities[key]) for key in list(self.entities)])
+        ents_str = ', '.join([repr(entity) \
+                              for entity in self.entity_list])
         ents_str = '(' + ents_str + ')'        
         return '<' + exps_str + ',\n' + ents_str + '>'
             
 class Expression:
     """Short for expression.
     A expression states a relation about some entities."""
-    def __init__(self, case, s_exp, weight=1.0, create_new_pred=True, evidences=None):
+    def __init__(self, case, s_exp, weight=1.0, create_new_pred=True, \
+                 evidences=None):
         pred_name = s_exp[0]
         arg_list = s_exp[1:]
         num_of_args = len(arg_list)
+        self.args = []
         if pred_name in current_vocab:
             self.predicate = current_vocab[pred_name]
         elif create_new_pred:
@@ -46,7 +111,8 @@ class Expression:
             print 'Unknown predicate', pred_name
             raise KeyError
         if current_vocab.check_arity(pred_name, num_of_args):
-            self.args = [case.entities[arg] for arg in arg_list]
+            for arg in arg_list:
+                self.args.append(case.add(arg))
         self.weight = weight
         self.evidences = evidences
         self.case = case
@@ -55,11 +121,19 @@ class Expression:
     def name(self):
         s_exp_list = [arg.name for arg in self.args]
         s_exp_list.insert(0, self.predicate.name)
-        return ' '.join(s_exp_list)
+        return '(' + ' '.join(s_exp_list) + ')'
 
+    @property
+    def list_form(self):
+        return [self.predicate.list_form] + \
+            [arg.list_form for arg in self.args]
+    
     def __repr__(self):
         return '<' + self.name + ', ' + repr(self.weight) + '>'
 
+    # def __hash__(self):
+    #     return hash(self.name)
+        
     def __deepcopy__(self, memo):
         new_copy = copy.copy(self)
         new_copy.evidences = copy.copy(self.evidences)
@@ -70,9 +144,15 @@ class Entity:
     def __init__(self, name):
         self.name = name
 
+    @property
+    def list_form(self):
+        return self.name
+
+    # def __hash__(self):
+    #     return hash(self.name)
+
     def __repr__(self):
         return '<' + self.name + '>'
-
     
 class Predicate:
     """Short for predicate."""
@@ -80,6 +160,10 @@ class Predicate:
         self.name = name
         self.arity = arity
 
+    @property
+    def list_form(self):
+        return self.name
+        
     def __repr__(self):
         return '<' + self.name + '>'
         
@@ -121,8 +205,20 @@ class Vocabulary:
         return '<' + repr(self.p_dict) + '>'
 
 current_vocab = Vocabulary()
-            
+
+
 #test examples
+
+class Test:
+    def __init__(self, i):
+        self.i = i
+
+    def __hash__(self):
+        return hash(self.i)
+
+t = Test(1)
+d = {t: 2}
+
 v = Vocabulary()
 v.add('r1', 2)
 print v['r1'].name
@@ -130,12 +226,34 @@ print v['r1'].name
 print v.check_arity('r1', 2)
 print v.check_arity('r1', 1)
 
-sc = StructCase([(['r1', 'e1', 'e2'], 2.0), (['r2', 'e1', 'e2'], 3.0)])
+sc = StructCase([(['r1', 'e1', 'e2'], 2.0),
+                 (['r2', 'e1', 'e2'], 3.0)])
 sc2 = copy.copy(sc)
 sc3 = copy.deepcopy(sc)
-print sc2.expressions == sc.expressions
-print sc3.expressions == sc.expressions
-print sc3.expressions['r1 e1 e2'].predicate == sc.expressions['r1 e1 e2'].predicate
+print sc3
+print sc3.items
+print sc2.items == sc.items
+print sc3.items == sc.items
+print sc3['(r1 e1 e2)'].predicate == sc['(r1 e1 e2)'].predicate
 
-water_flow = StructCase([(['greaterThan', 'bucket_1', 'bucket_2'], 2.0), (['flow', 'bucket_1', 'bucket_2', 'tube'], 3.0), (['greaterThan', 'bucket_2', 'bucket_1'], 2.0)])
-heat_flow = StructCase([(['greaterThan', 'ball_1', 'ball_2'], 2.0), (['flow', 'ball_1', 'ball_2', 'stick'], 3.0)])
+water_flow = StructCase( \
+    [(['greaterThan', 'bucket_1', 'bucket_2'], 2.0), \
+     (['flow', 'bucket_1', 'bucket_2', 'tube'], 3.0), \
+     (['greaterThan', 'bucket_2', 'bucket_1'], 2.0), \
+     (['cause', \
+       ['greaterThan', 'bucket_1', 'bucket_2'], \
+       ['flow', 'bucket_1', 'bucket_2', 'tube']], 1.0), \
+     (['cause', \
+       ['greaterThan', 'bucket_2', 'bucket_1'], \
+       ['flow', 'bucket_1', 'bucket_2', 'tube']], 1.0)])
+
+heat_flow = StructCase(
+    [(['greaterThan', 'ball_1', 'ball_2'], 2.0), \
+     (['flow', 'ball_1', 'ball_2', 'stick'], 3.0), \
+     (['cause', \
+       ['greaterThan', 'ball_1', 'ball_2'], \
+       ['flow', 'ball_1', 'ball_2', 'stick']], 1.0)])
+     # (['cause', \
+     #   ['greaterThan', 'ball_2', 'ball_1'], \
+     #   ['flow', 'ball_1', 'ball_2', 'stick']], 1.0)])
+
